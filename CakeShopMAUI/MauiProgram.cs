5cd9eb64872dd142340af8306bd479db621763fd
Nilesh Chaudhari <chaudharinilesh56@gmail.com>
2023-08-23 11:51:23 +0530

    1.Removed unwanted projects types. 2.Added communitytoolkit MAUI and MVVM

--- CakeShopMAUI/MauiProgram.cs
+++ CakeShopMAUI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace CakeShopMAUI;
+﻿using CommunityToolkit.Maui;
+
+namespace CakeShopMAUI;
 
 public static class MauiProgram
 {
@@ -11,7 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			}).UseMauiCommunityToolkit();
 
 		return builder.Build();
 	}
