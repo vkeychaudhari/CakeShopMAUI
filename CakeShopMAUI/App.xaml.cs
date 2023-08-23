ede245d115b272f323851735cdd70c5e040e0f30
Nilesh Chaudhari <chaudharinilesh56@gmail.com>
2023-08-23 10:45:08 +0530

    Initial project commited

---
+++ CakeShopMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CakeShopMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CakeShopMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CakeShopMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
