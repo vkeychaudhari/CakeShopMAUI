efc4a14ff23efcd84c1afde889cf5a9e8bcf32a8
Nilesh Chaudhari <chaudharinilesh56@gmail.com>
2023-08-23 22:17:59 +0530

    Getstarted UI designed.

---
+++ CakeShopMAUI/Pages/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CakeShopMAUI.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
--- CakeShopMAUI/Pages/MainPage.xaml.cs
+++ CakeShopMAUI/Pages/MainPage.xaml.cs
@@ -2,23 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
 }
 
