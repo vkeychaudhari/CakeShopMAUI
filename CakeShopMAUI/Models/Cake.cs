03735ae2c66d5a711e709880b340d9c2da198965
Nilesh Chaudhari <chaudharinilesh56@gmail.com>
2023-08-24 23:06:59 +0530

    Added cakes images and cake properties.

--- CakeShopMAUI/Models/Cake.cs
+++ CakeShopMAUI/Models/Cake.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace CakeShopMAUI.Models
 {
-    internal class Cake
+    public partial class Cake : ObservableObject
     {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public double Price { get; set; }
+
+        [ObservableProperty,NotifyPropertyChangedFor(nameof(Amount))]
+        private int _cartQuantity;
+
+        public double Amount => CartQuantity * Price;
+
+        public Cake Clone() => MemberwiseClone() as Cake;   
+        
+                
+
     }
 }
